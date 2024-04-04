f10dcfa14c690cac599625118357951d22dc2d98
WillNoon <62916396+WillNoon@users.noreply.github.com>
2024-04-04 16:09:22 +1100

    Validation

--- LoginFuctions.py
+++ LoginFuctions.py
@@ -8,18 +8,27 @@ def checkUser(userId,userPassword):
             if record['userId'] == userId:
                 if record['password'] == userPassword:
                     return (True, userId)
-                return (False,"Error, incorrect password")
-        return (False,"Error, account does not exist")
+                return (False,"incorrect password")
+        return (False,"account does not exist")
 
 
 def createAccount(userId,userName,userPassword):
     with open('db.json', 'r') as f:
         data = json.load(f)
     if data:
-        data.append({"userId":userId,"name":userName,"password":userPassword})
+        accountExists = False
+        for account in data:
+            if account["userId"] == userId:
+                accountExists = True
+                break
+        if not accountExists:
+            data.append({"userId":userId,"name":userName,"password":userPassword})
+        else:
+            return (False, 'account already exists')
     with open('db.json', 'w') as f:
         try: 
             json.dump(data, f, indent=2)
-            return True
+            return (True, '')
         except Exception as e:
-            return False
+            return (False, 'Error creatin account')
+        
